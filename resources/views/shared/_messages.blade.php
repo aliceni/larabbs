725a6ea0a46f9cf7e4e3e2f1836fafcfcba6f73c
aliceni <aliceni0522@gmail.com>
2020-05-21 07:13:32 +0000

    用户可以编辑话题

--- resources/views/shared/_messages.blade.php
+++ resources/views/shared/_messages.blade.php
@@ -3,6 +3,7 @@
     <div>
       <p class="alert alert-{{ $msg }}">
         {{ session()->get($msg) }}
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
       </p>
     </div>
   @endif
